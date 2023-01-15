455c4e5057360882a774e41c4500b8808bdda2de
Marlon Vasquez <vmarlon007@gmail.com>
2023-01-15 18:40:28 -0400

    add button update a los views

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView, BlogDetailView
+from .views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView
 
 
 app_name='blog'
@@ -7,5 +7,6 @@ app_name='blog'
 urlpatterns = [
     path('',BlogListView.as_view(), name='home'),
     path('create/', BlogCreateView.as_view(), name='create'),
-    path('<int:pk>/',BlogDetailView.as_view(), name='detail')
+    path('<int:pk>/',BlogDetailView.as_view(), name='detail'),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name='update')
 ]
