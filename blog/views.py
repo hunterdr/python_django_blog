1ec32563958c0fefd011abca70c47fdf7f291381
Marlon Vasquez <vmarlon007@gmail.com>
2023-01-15 16:20:56 -0400

    add configuracion de formulario

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import PostCreateForm
+from .models import Post
 
 # Create your views here.
 class BlogListView(View):
@@ -13,16 +14,27 @@ class BlogListView(View):
 
 class BlogCreateView(View):
     def get(self, request, *args, **kwargs):
+        form=PostCreateForm
         context ={
+            'form':form
 
         }
         return render(request, 'blog_create.html', context)
 
     def post(self, request, *args, **kwargs):
+        if request.method=='POST':
+            form = PostCreateForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                content = form.cleaned_data.get('content')
+
+                p, create = Post.objects.get_or_create(title=title, content=content)
+                p.save()
+                return redirect ('blog:home')
+
         context ={
 
         }
         return render(request, 'blog_create.html', context)
 
 
-        
